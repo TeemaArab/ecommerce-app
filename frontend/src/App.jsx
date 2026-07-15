9080def4867600e498bd48e1939610d5a9ff8dc6
Fatima <fatima.arab@gmail.com>
2026-07-14 22:38:33 -0400

    Navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,13 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] '>
       
+      <Navbar />
 
       <Routes>
          <Route path='/' element={<Home />} />
@@ -24,7 +26,7 @@ const App = () => {
          <Route path='/place-order' element={<PlaceOrder />} />
          <Route path='/orders' element={<Orders />} />
          <Route path='/contact' element={<Contact />} />
-         <Route path='/product/:id' element={<Product />} />
+         <Route path='/product/:productId' element={<Product />} />
       </Routes>
     </div>
   )
