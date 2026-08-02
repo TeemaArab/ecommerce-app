1f03ca5727b2cde7ba979eccb538e592033e042a
Fatima <fatima.arab@gmail.com>
2026-08-02 19:02:03 -0400

    user router created

---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+
+// route for user login
+const loginUser = async(req,res)=>{
+   
+}
+
+
+//route for user registration
+
+const registerUser = async(req,res)=>{
+   res.json({message:"User registered successfully"})
+}
+
+//route for admin login
+const adminLogin = async(req,res) =>{
+
+}
+
+
+export {loginUser, registerUser, adminLogin};
