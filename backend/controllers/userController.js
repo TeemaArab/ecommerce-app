f11a7680aa9e520710e52b4cbee446739e084701
Fatima <fatima.arab@gmail.com>
2026-08-04 20:04:33 -0400

    user registration function in usercontroller completed and API tested successfully in Postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,15 @@
 
+import bcrypt from 'bcrypt';
+import validator from 'validator';
+import jwt from 'jsonwebtoken';
+import userModel from "../models/userModel.js";
+
+
+// to generate token 
+const createToken = (id) =>{
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+
 // route for user login
 const loginUser = async(req,res)=>{
    
@@ -8,7 +19,44 @@ const loginUser = async(req,res)=>{
 //route for user registration
 
 const registerUser = async(req,res)=>{
-   res.json({message:"User registered successfully"})
+   try{
+    const {name,email,password} = req.body;
+
+    // checkin user already exists or not
+    const userExists = await userModel.findOne({email});
+    if(userExists){
+        return res.json({success:false, message:'User already exists'})
+    }
+    // now validating email format & strong password
+    if(!validator.isEmail(email)){
+        return res.json({success:false, message:'Invalid email format'})
+    }
+
+      if(password.length < 8 ){
+        return res.json({success:false, message:'Please enter a strong password with minimum 8 characters'})
+    }
+
+    // hashing the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //creating a new user
+    const newUser = new userModel({
+        name,
+        email,
+        password:hashedPassword
+    })
+
+    const user = await newUser.save();
+
+    //create a token
+    const token = createToken(user._id);
+      res.json({success:true, message:'User registered successfully', token})
+
+   }catch(error){
+    console.log(error);
+    return res.json({success:false, message:error.message})
+   }
 }
 
 //route for admin login
