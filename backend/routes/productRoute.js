7ebd889872ca0e9a7e22528bb9ce93bda4362b0f
Fatima <fatima.arab@gmail.com>
2026-08-20 21:07:54 -0400

    admin authentication done and tested successfully

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import { addproduct, removeProduct, listProducts, singleProduct } from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 
 //creating router
 const productRouter = express.Router();
 
 // router for adding a new product
-productRouter.post('/add',upload.fields([{name: 'image1', maxCount: 1},{name: 'image2', maxCount: 1},{name: 'image3', maxCount: 1},{name: 'image4', maxCount: 1}]), addproduct);
+productRouter.post('/add',adminAuth,upload.fields([{name: 'image1', maxCount: 1},{name: 'image2', maxCount: 1},{name: 'image3', maxCount: 1},{name: 'image4', maxCount: 1}]), addproduct);
 
 productRouter.get('/list', listProducts);
 
-productRouter.delete('/remove/:id', removeProduct);
+productRouter.delete('/remove/:id',adminAuth, removeProduct);
 
 productRouter.get('/single/:id', singleProduct);
 
